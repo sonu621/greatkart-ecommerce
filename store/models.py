2a3f58570b70b408a468d7f8c0829ceeacaaac63
sonu621 <sanjiwgpt55@gmail.com>
2026-06-14 16:28:30 +0300

    Set up ecommerce application foundation

---
+++ store/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+from category.models import Category
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(max_length=300, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+
+    def __str__(self):
+        return self.product_name
