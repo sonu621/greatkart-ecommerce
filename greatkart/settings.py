0489346bb5e5bb7f4b875eb380df6d7d4a3b1fde
Sonu Gupta <sonu@Sonus-MacBook-Air.local>
2026-07-10 21:22:39 +0300

    Configure ALLOWED_HOSTS for Elastic Beanstalk

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost",
+    cast=lambda v: [host.strip() for host in v.split(",")],
+)
 
 
 # Application definition
